e90584737f804fe67be163cb2dff28ea70bd2e8f
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-23 00:49:03 +0500

    Home page in progress

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('.carousel-1').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },    
+        768:{
+            items:2
+        },
+        920:{
+            items:3
+        },
+        1300:{
+            items:4
+        }
+    }
+})
+
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },    
+        620:{
+            items:2
+        },
+        920:{
+            items:3
+        }
+    }
+})
