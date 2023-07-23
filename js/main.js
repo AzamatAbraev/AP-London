700a1d5b0c677304de718379be86187579df1ee0
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-24 02:53:09 +0500

    final draft ready

--- js/main.js
+++ js/main.js
@@ -18,9 +18,35 @@ $('.carousel-1').owlCarousel({
     }
 })
 
-$('.owl-two').owlCarousel({
+$('.owl-three').owlCarousel({
     loop:true,
     margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        }, 
+        576:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:3
+        }
+    }
+})
+
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:32,
     nav:true,
     responsive:{
         0:{
@@ -29,8 +55,45 @@ $('.owl-two').owlCarousel({
         620:{
             items:2
         },
-        920:{
+        992:{
             items:3
         }
     }
 })
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+// Navbar Shrink //
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("nav-shrink");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+// Dark Mode //
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+// Back To Top
+
